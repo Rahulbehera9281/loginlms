c3f750f0de772a31e034a8f541a522ac44b68ec1
Rahulbehera9281 <141346046+Rahulbehera9281@users.noreply.github.com>
2024-02-22 16:31:07 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="LMS._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div style="height:42px;border:1px solid black;border-radius:4px;padding:3px 3px 3px 3px;background-color:#e4e4e4;">
+        <ul style="display:flex;">
+            <li style="color:#1C3481; font-size:31px;font-weight:800;padding:4px;">RAMTeCH</li>
+            <li style=" font-size:31px;font-weight:200;margin-left:1400px;">Leave Management System</li>
+        </ul>
+    </div>
+    <center>
+          <div style="border:1px solid #1C3481;height:49px;width:294px; background-color:#1C3481;margin-top:242px;">
+        <div style="margin-top:10px;"><strong style="color:white;font-size:24px ;margin-left:21px;">Login To LMS</strong>&nbsp;&nbsp;&nbsp;&nbsp;</div>
+    </div>
+        <div style="height:269px;width:294px;border:1px solid #1C3481;margin-top:3px;">
+            <div style="float:left;margin-left:28px;margin-top:20px;">
+                <strong style="float:left;font-family:Sans-serif;margin-left:12px;">Login with your PM Account</strong><br/>
+                
+                <strong style="float:left;margin-top:22px;color:blue;margin-left:12px;">Login Name / User Name</strong><br />
+                
+              <div style="margin-top:31px;">
+                  <asp:TextBox ID="TextBox1" runat="server" Height="31px" Width="198px"></asp:TextBox><br />
+              </div>  
+
+                 <strong style="float:left;margin-top:17px;color:blue;margin-left:12px;">Password</strong><br />
+                <div style="margin-top:25px;">
+                    <asp:TextBox ID="TextBox2" runat="server" Height="31px" Width="198px"></asp:TextBox>
+                </div>
+                <div style="margin-top:10px;">
+                    <asp:Button ID="Button1" runat="server" Text="Login" Height="32px" Width="99px" BackColor="Blue" Font-Bold="True" ForeColor="White" OnClick="Button1_Click" />
+                </div>
+                
+                
+
+            </div>
+        </div>
+    </center>
+  
+   
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,74 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LMS
+{
+    public partial class _Default : Page
+    {
+        string dbpath;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            dbpath = Server.MapPath("~/MySQL/credential.xlsx");
+            string userid = TextBox1.Text;
+            string password = TextBox2.Text;
+            if(userid=="" && password=="")
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('Please Enter Both User Id and Password');", true);
+                return;
+            }
+            if (userid == "" || password == "")
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('You are missing User Id or Password');", true);
+                return;
+            }
+            bool isvalidcredential = isvalidcredentiall(userid, password, dbpath);
+            if(isvalidcredential)
+            {
+                string script = "alert('Login Successful'); window.setTimeout(function(){ window.location.href = '" + ResolveUrl("~/WebForm1.aspx") + "'; },);";
+                ClientScript.RegisterStartupScript(this.GetType(), "foundAlert", script, true);
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "Alert", "alert('Invalid Credential');", true);
+            }
+        }
+        private bool isvalidcredentiall(string user,string pass,string dbpath)
+        {
+            FileInfo fileInfo = new FileInfo(dbpath);
+            if (!fileInfo.Exists)
+            {
+                throw new FileNotFoundException("The specified Excel file was not found.", dbpath);
+            }
+
+            using (ExcelPackage package = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[0]; // Assuming the data is in the first worksheet
+                int rowCount = worksheet.Dimension.Rows;
+
+                for (int row = 1; row <= rowCount; row++)
+                {
+                    string excelUser = worksheet.Cells[row, 1].Text; // Assuming the username is in the first column
+                    string excelPass = worksheet.Cells[row, 2].Text; // Assuming the password is in the second column
+
+                    if (excelUser.Equals(user, StringComparison.OrdinalIgnoreCase) && excelPass == pass)
+                    {
+                        return true; // Credentials match
+                    }
+                }
+            }
+
+            return false; // No matching credentials found
+        }
+    }
+}
